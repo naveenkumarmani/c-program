d06532cb0fc87184cc595ff9db825fbed0d48b09
naveenkumarmani <46814160+naveenkumarmani@users.noreply.github.com>
2019-01-23 15:47:12 +0530

    Create beginner 1.c

---
+++ beginner 1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+  int a;
+  scanf("%d",&a);
+  if(a>0)
+  {
+    printf("the number is positive");
+  }
+  else if(a<0)
+  {
+    printf("the number is negative");
+  }
+  else
+  {
+    printf("the number is zero");
+  }
+
+}
