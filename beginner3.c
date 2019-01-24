a9a0252677f8832246aafc4f1ecedf6c005a844e
naveenkumarmani <46814160+naveenkumarmani@users.noreply.github.com>
2019-01-24 16:03:18 +0530

    Update beginner3.c

--- beginner3.c
+++ beginner3.c
@@ -3,7 +3,13 @@ void main()
 {
   char A;
   scanf("%c",&A);
+  if(A>=65&&A<=90||A>=97&&A<=122)
+  {
   if(A=='a'||A=='e'||A=='i'||A=='o'||A=='u')
+  {
+    printf("vowel");
+  }
+  else if(A=='A'||A=='E'||A=='I'||A=='O'||A=='U')
   {
     printf("vowel");
   }
@@ -12,3 +18,7 @@ void main()
     printf("consonent");
   }
 }
+else
+{
+  printf("invalid");
+}}
