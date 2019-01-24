9c4e2c401d029396ffbe1794989e32b9c1290bbb
naveenkumarmani <46814160+naveenkumarmani@users.noreply.github.com>
2019-01-24 15:50:09 +0530

    Create beginner3.c

---
+++ beginner3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+  char A;
+  scanf("%c",&A);
+  if(A=='a'||A=='e'||A=='i'||A=='o'||A=='u')
+  {
+    printf("vowel");
+  }
+  else
+  {
+    printf("consonent");
+  }
+}
