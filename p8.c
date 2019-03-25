66d529fc0af13743a390b710c3de1b67e60b0229
naveenkumarmani <46814160+naveenkumarmani@users.noreply.github.com>
2019-03-25 12:29:09 +0530

    Create p8.c

---
+++ p8.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+  char a[200];
+  int i;
+  scanf("%[^\n]s",a);
+  for(i=0;a[i]!='\0';i++)
+  {
+    if(i==0)
+    {
+      if(a[i]>='a'&&a[i]<='z')
+      a[i]-=32;
+      continue;
+    }
+    if(a[i]==' ')
+    {
+      ++i;
+        if(a[i]>='a'&&a[i]<='z')
+         a[i]-=32;
+         continue;
+    }
+    else 
+         {
+         if(a[i]>='A'&&a[i]<='Z')
+         a[i]+=32;
+         }
+  }
+ 
+printf("%s",a);
+}
